e1a062c675ab3812a5f05262df4485994323388a
Stinkypirate15 <13579268cd@gmail.com>
2025-06-24 18:46:51 -0400

    added sort function and better formating

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from stats import get_book_text, split_text,characters_amount
+from stats import get_book_text, split_text,characters_amount,sort_characters
 def main():
     
     filepath="books/frankenstein.txt"
     returned_getbook = ( get_book_text(filepath))
-    print(get_book_text(filepath))
+    print(f"============ BOOKBOT ============\n")
+    print(f"Analyzing book found at {filepath}...\n")
+    print("----------- Word Count ----------\n")
+    #print(get_book_text(filepath))
+    print(f"Found {split_text(returned_getbook)} total words\n")
+    print("--------- Character Count -------")
+    
+    #print(characters_amount(returned_getbook))
+    sorted_chars = sort_characters(characters_amount(returned_getbook))
+    for char_dict in sorted_chars:
+         if char_dict["char"].isalpha():
+            print(f"{char_dict['char']}: {char_dict['num']}")
+           
+        
+
+   
     
     
-    
-    print(f"{split_text(returned_getbook)} words found in the document")
-    print(characters_amount(returned_getbook))
-    
-    
-    
+
 
 
 
--- stats.py
+++ stats.py
@@ -9,15 +9,32 @@ def split_text(returned_getbook):
     return max_len
 
 def characters_amount(returned_getbook):
-    char_characters={}
-    characters_lowered=returned_getbook.lower()
+    char_characters={}#creates empty dict
+    characters_lowered=returned_getbook.lower()#makes every letter in book lwoer case
     for i in characters_lowered:
         character_check=i in char_characters
-        if character_check:
+        if character_check:#if character is detected in list adds count to it
             char_characters[i] += 1
         else:
-            char_characters[i]= 1
+            char_characters[i]= 1#if character is not detected in list adds it to list
     return char_characters
+
+def sort_characters(char_characters):
+    def sort_on(dict_Def):#creates function allowing for sorting
+        return dict_Def["num"]
+    
+    arrangement=[]#Creates empty list
+   
+    
+    
+    for char,count in char_characters.items():#defines what were looking for in dict
+    
+        
+        dict_Def={"char":char,"num": count}#creates format for dict
+        arrangement.append(dict_Def)#appends dict to list
+    arrangement.sort(reverse=True, key=sort_on)#sorts from highest to lowest
+        
+    return arrangement
 
       
    
